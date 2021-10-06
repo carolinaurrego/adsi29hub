9e865881d88f021f4e76a975bca31fd60eff8865
curregof <dcurrego35@misena.edu.co>
2021-10-05 19:57:36 -0500

    segunda version

--- clases/funciones/index.php
+++ clases/funciones/index.php
@@ -1,17 +1,16 @@
-<! DOCTYPE html>
-< html  lang = " en " >
-< cabeza >
-    < meta  charset = " UTF-8 " >
-    < meta  http-equiv = " X-UA-Compatible "  content = " IE = edge " >
-    < meta  name = " viewport "  content = " width = device-width, initial-scale = 1.0 " >
-    < título > Documento </ título >
-</ cabeza >
-< cuerpo >
-    < form  action = " resolver.php "  method = " POST " >
-        Señor usuario, por favor digite la cantidad de numeros que requiera:
-        < Entrada de  nombre = " numero "  tipo = " número "  min = " 1 "  max = " 100 "  paso = " 0.5 " > < br >
-        < Botón de  tipo = " submit " > Resolver </ botón > < br >
-    </ formulario >
-</ cuerpo >
-</ html >
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+        <label>por favor ingrese los números:</label>
+        <input type="number"name="valor"value="valor"><br><br>
+        <input type="submit"value="Calcular">
+    </form>
+    </body>
+</html>
