61aa08fdb4ee696ce794dec246f7b13195650f01
curregof <dcurrego35@misena.edu.co>
2021-10-05 19:54:07 -0500

    primer version

---
+++ clases/funciones/index.php
@@ -0,0 +1,17 @@
+<! DOCTYPE html>
+< html  lang = " en " >
+< cabeza >
+    < meta  charset = " UTF-8 " >
+    < meta  http-equiv = " X-UA-Compatible "  content = " IE = edge " >
+    < meta  name = " viewport "  content = " width = device-width, initial-scale = 1.0 " >
+    < título > Documento </ título >
+</ cabeza >
+< cuerpo >
+    < form  action = " resolver.php "  method = " POST " >
+        Señor usuario, por favor digite la cantidad de numeros que requiera:
+        < Entrada de  nombre = " numero "  tipo = " número "  min = " 1 "  max = " 100 "  paso = " 0.5 " > < br >
+        < Botón de  tipo = " submit " > Resolver </ botón > < br >
+    </ formulario >
+</ cuerpo >
+</ html >
+
