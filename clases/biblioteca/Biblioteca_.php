042bdb6281cd89b0bfc4c3b4901e655c347177d1
curregof <dcurrego35@misena.edu.co>
2021-10-06 22:24:05 -0500

    cuarta version herencia

--- clases/biblioteca/Biblioteca_.php
+++ clases/biblioteca/Biblioteca_.php
@@ -1,36 +1,59 @@
-<?php 
-class Biblioteca {
-
-    protected $material;
-    protected $nombreBiblioteca;
-    protected $direccion;
-
-    public function __construct($material,$nombreBiblioteca,$direccion){
-        $this->material = $material;
-        $this->nombreBiblioteca = $nombreBiblioteca;
-        $this->direccion       = $direccion;
-    }
-
-    function setter($nomAtributo,$valor){
-        $this->nomAtributo = $valor;
-    }
-
-    function getter($datos){
-        return $this->datos;
-    }
-}
-
-$biblioteca = new Biblioteca("libros","arango","simon Bolivar");
-
-$biblioteca->setter('material',"libros");
-$biblioteca->setter('nombreBiblioteca','arango');
-$biblioteca->setter('direccion','Simon Bolivar');
-
-echo $biblioteca->getter("material");
-echo $biblioteca->getter("nombreBiblioteca");
-echo $biblioteca->getter("direccion");
-
-
-
 
 ?>
+<html>
+<center>
+<?php
+echo '<br>';
+echo '<h1><strong>Revista</strong></h1>';
+include ('revista.php');
+class Biblioteca{
+    private $revista;    
+
+    function __construct(){
+        $this->revista=array();
+    }
+    
+    function adicionar($material){
+        array_push($this->revista,$material);
+    }
+
+    function verMateriales(){
+        for ($i=0; $i <count($this->revista) ; $i++) { 
+            echo $this->revista[$i]->gettema();
+        }
+    }
+    
+}
+
+$dator=new Biblioteca();
+$dator->adicionar($ob);
+$dator->verMateriales();
+echo '<br>';
+echo '<h1><strong>Libro</strong></h1>';
+include ('libros.php');
+class Biblioteca1{
+    private $libro;    
+
+    function __construct(){
+        $this->libro=array();
+    }
+    
+    function adicionar($material1){
+        array_push($this->libro,$material1);
+    }
+
+    function verMateriales(){
+        for ($i=0; $i <count($this->libro) ; $i++) { 
+            echo $this->libro[$i]->getAutor();
+        }
+    }
+    
+}
+
+$datol=new Biblioteca1();
+$datol->adicionar($ob2);
+$datol->verMateriales();
+
+?>
+</center>
+</html>
