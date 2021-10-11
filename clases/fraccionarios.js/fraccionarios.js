d3dd9b8f1cc3ce2354e1b532807bc0fc2d826d0b
curregof <dcurrego35@misena.edu.co>
2021-10-10 23:28:32 -0500

    cambios

--- clases/fraccionarios.js/fraccionarios.js
+++ clases/fraccionarios.js/fraccionarios.js
@@ -1,44 +1,38 @@
-var  frac = {
-   n1 : 1 ,
-   n2 : 4 ,
-   d1 : 7 ,
-   d2 : 3 ,
-   suma : function ( ) {
-       numeradorS1 = esto . n1  *  esto . d2 ;
-       numeradorS2 = esto . n2  *  esto . d1 ;
-       numeradorSF = numeradorS1  +  numeradorS2 ;
-       denominadorS = esto . d1  *  esto . d2 ;
-       return  'El resultado de la suma es' +  numeradorSF  +  '/'  +  denominadorS ;
-   } ,
-   resta : function ( ) {
-       numeradorR1 = esto . n1  *  esto . d2 ;
-       numeradorR2 = esto . n2  *  esto . d1 ;
-       numeradorRF = numeradorS1  -  numeradorS2 ;
-       denominadorR = esto . d1  *  esto . d2 ;
-       return  'El resultado de la resta es' +  numeradorRF  +  '/'  +  denominadorR ;
-   } ,
-   _multi: function () {
-      numeradorM1 = esto.n1 * esto.n2;
-      numeradorM2 = esto.d1 * esto.d2;
-      return 'El resultado de la multiplicacion es' + numeradorM1 + '/' + numeradorM2;
-   } ,
-   get multi() {
-      return this._multi;
+var frac={
+   n1:8,
+   n2:6,
+   d1:10,
+   d2:7,
+   suma:function(){
+       numeradorS1=this.n1 * this.d2;
+       numeradorS2=this.n2 * this.d1;
+       numeradorSF=numeradorS1 + numeradorS2;
+       denominadorS=this.d1 * this.d2;
+       return 'El resultado de la suma es '+ numeradorSF + ' / ' + denominadorS;
    },
-   set multi(value) {
-      this._multi = value;
+   resta:function(){
+       numeradorR1=this.n1 * this.d2;
+       numeradorR2=this.n2 * this.d1;
+       numeradorRF=numeradorS1 - numeradorS2;
+       denominadorR=this.d1 * this.d2;
+       return 'El resultado de la resta es '+ numeradorRF + ' / ' + denominadorR;
    },
-   divi : function ( ) {
-       numeradorD1 = esto . n1  *  esto . d2 ;
-       numeradorD2 = esto . n2  *  esto . d1 ;
-       return  'El resultado de la division es' +  numeradorD1  +  '/'  + numeradorD2 ;
+   multi:function(){
+       numeradorM1=this.n1 * this.n2;
+       numeradorM2=this.d1 * this.d2;
+       return 'El resultado de la multiplicacion es '+ numeradorM1 + ' / ' +numeradorM2;
+   },
+   divi:function(){
+       numeradorD1=this.n1 * this.d2;
+       numeradorD2=this.n2 * this.d1;
+       return 'El resultado de la division es '+ numeradorD1 + ' / ' +numeradorD2;
    }
 }
 
-consola . log ( frac . suma ( ) ) ;
+console.log(frac.suma());
 
-consola . log ( frac . resta ( ) ) ;
+console.log(frac.resta());
 
-consola . log ( frac . multi ( ) ) ;
+console.log(frac.multi());
 
-consola . log ( frac . divi ( ) ) ;
+console.log(frac.divi());
